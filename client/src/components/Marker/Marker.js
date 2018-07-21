924e194b77472140ab9e63735da27c288d08333d
TAYLOR YAO <yaotaylor25@gmail.com>
2018-07-21 11:09:09 -0400

    url marker conflict fixed2

--- client/src/components/Marker/Marker.js
+++ client/src/components/Marker/Marker.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Marker = props =>(
-    <div style={{backgroundColor: 'red', width: '5px', height: '5px'}}>{props.text}</div>
+    <div>
+         <img  style={{maxWidth: "25px"}}src="http://www.iconarchive.com/download/i97927/flat-icons.com/flat/Beer.ico" alt="BEER"/>
+    </div>
 )
 
 export default Marker;
